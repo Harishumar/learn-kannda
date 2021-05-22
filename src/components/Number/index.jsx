39aed50f0e3e71c8e30195ac01d58fe10fee85bd
Harishumar <harisumar123@.com>
2021-05-22 17:17:23 +0530

    create number component elements

--- src/components/Number/index.jsx
+++ src/components/Number/index.jsx
@@ -1,4 +1,104 @@
 const Numbers = () => {
-    return (<p>Welcome to NumberScreen !!!</p>);
+    return (<><p>Welcome to NumberScreen !!!</p>
+    <div className="row" >
+    <div className="col s8 m1">
+      <div className="card-panel #29b6f6 light-blue lighten-1">
+      <span className="white-text"><i className="material-icons right">೦</i>
+       <br/><i className="material-icons right">0</i>
+       <br/><i className="material-icons right">sonne (ಸೊನ್ನೆ)</i></span> 
+      </div>
+    </div>
+  </div>
+  <div className="row" >
+    <div className="col s8 m1">
+      <div className="card-panel #29b6f6 light-blue lighten-1">
+      <span className="white-text"><i className="material-icons right">೧</i>
+       <br/><i className="material-icons right">1</i>
+       <br/><i className="material-icons right">ondu (ಒಂದು)</i></span> 
+      </div>
+    </div>
+  </div>
+  <div className="row" >
+    <div className="col s8 m1">
+      <div className="card-panel #29b6f6 light-blue lighten-1">
+      <span className="white-text"><i className="material-icons right">೨	</i>
+       <br/><i className="material-icons right">2</i>
+       <br/><i className="material-icons right">eraḍu (ಎರಡು)</i></span> 
+      </div>
+    </div>
+  </div>
+  <div className="row" >
+    <div className="col s8 m1">
+      <div className="card-panel #29b6f6 light-blue lighten-1">
+      <span className="white-text"><i className="material-icons right">೩</i>
+       <br/><i className="material-icons right">3</i>
+       <br/><i className="material-icons right">mūru (ಮೂರು)</i></span> 
+      </div>
+    </div>
+  </div>
+  <div className="row" >
+    <div className="col s8 m1">
+      <div className="card-panel #29b6f6 light-blue lighten-1">
+      <span className="white-text"><i className="material-icons right">೪</i>
+       <br/><i className="material-icons right">4</i>
+       <br/><i className="material-icons right">nālku (ನಾಲ್ಕು)</i></span> 
+      </div>
+    </div>
+  </div>
+  <div className="row" >
+    <div className="col s8 m1">
+      <div className="card-panel #29b6f6 light-blue lighten-1">
+      <span className="white-text"><i className="material-icons right">೫</i>
+       <br/><i className="material-icons right">5</i>
+       <br/><i className="material-icons right">aidu (ಐದು)</i></span> 
+      </div>
+    </div>
+  </div>
+  <div className="row" >
+    <div className="col s8 m1">
+      <div className="card-panel #29b6f6 light-blue lighten-1">
+      <span className="white-text"><i className="material-icons right">೬</i>
+       <br/><i className="material-icons right">6</i>
+       <br/><i className="material-icons right">āru (ಆರು)</i></span> 
+      </div>
+    </div>
+  </div>
+  <div className="row" >
+    <div className="col s8 m1">
+      <div className="card-panel #29b6f6 light-blue lighten-1">
+      <span className="white-text"><i className="material-icons right">೭</i>
+       <br/><i className="material-icons right">7</i>
+       <br/><i className="material-icons right">ēḷu (ಏಳು)</i></span> 
+      </div>
+    </div>
+  </div>
+  <div className="row" >
+    <div className="col s8 m1">
+      <div className="card-panel #29b6f6 light-blue lighten-1">
+      <span className="white-text"><i className="material-icons right">೮</i>
+       <br/><i className="material-icons right">8</i>
+       <br/><i className="material-icons right">enṭu (ಎಂಟು)</i></span> 
+      </div>
+    </div>
+  </div>
+  <div className="row" >
+    <div className="col s8 m1">
+      <div className="card-panel #29b6f6 light-blue lighten-1">
+      <span className="white-text"><i className="material-icons right">೯</i>
+       <br/><i className="material-icons right">9</i>
+       <br/><i className="material-icons right">oṃbattu (ಒಂಬತ್ತು)	</i></span> 
+      </div>
+    </div>
+  </div>
+  
+  <div className="row" >
+    <div className="col s8 m1">
+      <div className="card-panel #29b6f6 light-blue lighten-1">
+      <span className="white-text"><i className="material-icons right">೧೦</i>
+       <br/><i className="material-icons right">10</i><br/><i className="material-icons right">hattu (ಹತ್ತು)</i></span> 
+      </div>
+    </div>
+  </div>
+    </>);
 }
 export default Numbers;
